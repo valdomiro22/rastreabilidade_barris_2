c838a10840e95f2bc96fa77e4cf837045f86d7bd
Valdomiro Santos <valdomiro.jose@outlook.com>
2026-03-12 17:41:53 -0300

    dependencias e firebase

---
+++ lib/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // final state = ref.watch(provider);
+    return const Scaffold(
+      body: Center(
+        child: Text('HomeScreen'),
+      ),
+    );
+  }
+}
